e7cbdffcd151472cff3c1d08c19dbe355eba053b
Jeetbhuva1019 <jeetbhuva70@gmail.com>
2025-01-21 13:35:26 +0530

    Day10

--- Day10(pr)/Route/route.js
+++ Day10(pr)/Route/route.js
@@ -2,16 +2,30 @@ const express = require('express');
 const route = express.Router();
 const control = require('../controller/control');
 const multer = require('../middleware/multer');
+const passport = require('../middleware/passport');
 
-route.get("/", control.login);
+route.get("/", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.redirect("/dashboard");
+    } else {
+        control.login(req, res);
+    }
+});
+route.post("/userLogin", 
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res) => {
+        console.log("Redirecting to /dashboard after successful login");
+        res.redirect('/dashboard');
+    }
+);
+
 route.get("/logout", control.logout);
-route.post("/userLogin", control.userLogin);
-route.get("/dashboard", control.dashboard);
-route.get("/addAdmin", control.addAdmin);
-route.get("/viewAdmin", control.viewAdmin);
+route.get("/dashboard", passport.checkAuth, control.dashboard);
+route.get("/addAdmin",passport.checkAuth, control.addAdmin);
+route.get("/viewAdmin", passport.checkAuth, control.viewAdmin);
 route.post("/addAdminData", multer, control.addAdminData);
-route.get("/deleteAdmin", multer, control.deleteAdmin);
-route.get("/editAdmin", multer, control.editAdmin);
+route.get("/deleteAdmin",multer , control.deleteAdmin);
+route.get("/editAdmin",passport.checkAuth,control.editAdmin);
 route.post("/updateAdmin", multer, control.updateAdmin);
 
 module.exports = route;
--- Day10(pr)/controller/control.js
+++ Day10(pr)/controller/control.js
@@ -3,42 +3,35 @@ const Admin = require('../model/adminSchema');
 const path = require('path');
 
 module.exports.login = (req, res) => {
-    if (req.cookies.adminData) {
-        res.redirect('/dashboard');
-    } else {
-        res.render("login");
-    }
+    res.render("login");
+
 }
 
 module.exports.logout = (req, res) => {
-    res.clearCookie("adminData");
+    req.session.destroy();
     res.redirect("/");
+
 }
 
 module.exports.userLogin = async (req, res) => {
-    console.log(req.body.email);
+    console.log("userLogin function called");
+    console.log("Login attempt for email:", req.body.email);
     let admin = await Admin.findOne({ email: req.body.email });
-    console.log(admin)
     if (admin) {
-        if (admin.password == req.body.passWord) {
-            res.cookie("adminData", admin);
-            res.redirect('/dashboard');
-        } else {
-            res.redirect("/");
-        }
+        console.log("Login successful for email:", req.body.email);
+        res.redirect("/dashboard");
     } else {
+        console.log("Login failed for email:", req.body.email);
         res.redirect("/");
     }
 }
 
-
 module.exports.dashboard = (req, res) => {
-    req.cookies.adminData ? res.render("dashboard") : res.redirect("/");
-
+    res.render("dashboard", { adminData: req.user });
 }
 
 module.exports.addAdmin = (req, res) => {
-    req.cookies.adminData ? res.render('addAdmin') : res.redirect("/");
+    res.render('addAdmin', { adminData: req.user });
 }
 
 module.exports.addAdminData = async (req, res) => {
@@ -51,28 +44,20 @@ module.exports.addAdminData = async (req, res) => {
     });
 }
 
-module.exports.viewAdmin = (req, res) => {
-    if (req.cookies.adminData) {
-        Admin.find().then((data) => {
-            res.render('viewAdmin', { data });
-        })
-    } else {
-        res.redirect("/");
-        alert("invaild email and password");
-    }
+module.exports.viewAdmin = async (req, res) => {
+    const data = await Admin.find();
+    res.render("viewAdmin", { data: Array.isArray(data) ? data : [], adminData: req.user });
 }
-
 
 module.exports.deleteAdmin = async (req, res) => {
     await Admin.findByIdAndDelete(req.query.id).then((data) => {
-        fs.unlinkSync(data.image);
         res.redirect("/viewAdmin");
     })
 }
 
 module.exports.editAdmin = async (req, res) => {
     const data = await Admin.findById(req.query.id);
-    res.render('editAdmin', { data });
+    res.render('editAdmin', { data, adminData: req.cookies.adminData });
 }
 
 module.exports.updateAdmin = async (req, res) => {
@@ -80,7 +65,14 @@ module.exports.updateAdmin = async (req, res) => {
     let singleData = await Admin.findById(req.body.id);
     req.file ? img = req.file.path : img = singleData.image;
     req.file && fs.unlinkSync(singleData.image);
-    await Admin.findByIdAndUpdate(req.body.id, { ...req.body, image: img }).then((data) => {
-        res.redirect("/viewAdmin");
-    })
+    req.body.image = img;
+    await Admin.findByIdAndUpdate(req.body.id, req.body);
+
+    if (req.cookies.adminData && req.body.id === req.cookies.adminData._id.toString()) {
+        let data = await Admin.findById(req.body.id);
+        if (data) {
+            res.cookie("adminData", data);
+        }
+    }
+    res.redirect("/viewAdmin");
 }
